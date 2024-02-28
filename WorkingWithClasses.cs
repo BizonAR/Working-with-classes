04d5e285e87763eeb5bf5f4d3b7852bc11d4862c
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 14:05:11 +0500

    Working with classes

---
+++ WorkingWithClasses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WorkingWithClasses
+{
+	internal class WorkingWithClasses
+	{
+		static void Main()
+		{
+			Player player = new Player("John", 100);
+
+			player.ShowInfo();
+		}
+	}
+
+	class Player
+	{
+		private string _name;
+		private int _score;
+
+		public Player(string name, int score)
+		{
+			_name = name;
+			_score = score;
+		}
+
+		public void ShowInfo()
+		{
+			Console.WriteLine($"Имя игрока - {_name}, его счёт - {_score}");
+		}
+	}
+}
